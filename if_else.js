c848d91b4814322f8d17ce911facd9f1af6b8d34
Mahesh <itstribe.mahi@gmail.com>
2022-08-16 17:14:49 +0530

    College ranks using if else

---
+++ if_else.js
@@ -0,0 +1,24 @@
+//even number
+//    var x=12;
+//     if(x%2==0){
+//        console.log(x, 'is even number');
+//     }
+//     else{
+//        console.log(x, 'is odd number');
+//     }
+
+//ranking on if else
+
+var x=1100;
+if(x<=10){
+    console.log('you got college A');
+}
+else if(x>10 && x<=50){
+    console.log('you got college B');
+}
+else if(x>50 && x<=122){
+    console.log('you got college c');
+}
+else{
+    console.log('you got nothing');
+}
